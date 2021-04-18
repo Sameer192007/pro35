8f3f8ecfb3aa380ca878aaf0b79fc48c7942c29b
Sameer192007 <75990855+Sameer192007@users.noreply.github.com>
2021-04-18 15:48:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+var balloon,balloonImage1,balloonImage2;
+var database;
+var position;
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  createCanvas(1500,700);
+  //database=firebase.database();
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonPosition=database.ref("balloon/position");
+    balloonPosition.on("value",readPosition,showerror);
+  textSize(20); 
+}
+
+// function to display UI   
+function draw() {
+  background(bg);
+  if(position !== undefined){
+    if(keyDown(LEFT_ARROW)){
+      updateHeight(-10,0)
+      balloon.addAnimation("hotAirBalloon",balloonImage2);
+      balloon.scale=balloon.scale -0.01;
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      updateHeight(10,0)
+      balloon.addAnimation("hotAirBalloon",balloonImage2);
+      balloon.scale=balloon.scale -0.01;
+    }
+    else if(keyDown(UP_ARROW)){
+      updateHeight(0,-10)
+      balloon.addAnimation("hotAirBalloon",balloonImage2);
+      balloon.scale=balloon.scale -0.01;
+    }
+    else if(keyDown(DOWN_ARROW)){
+      updateHeight(0,+10)
+      balloon.addAnimation("hotAirBalloon",balloonImage2);
+      balloon.scale=balloon.scale -0.01;
+    }
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function readPosition(data){
+  position=data.val();
+  ball.x = position.x;
+  ball.y = position.y;
+}
+function updateHeight(x,y){
+  database.ref("ball/position").set({
+      'x': position.x+x,
+      'y': position.y+y
+  })
+}
+function showerror(){
+  console.log("error");
+}
